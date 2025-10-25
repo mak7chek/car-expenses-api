662612297285b175f25cb0911c9eabfec47251a4
mak7 <mak77chek@gmail.com>
2025-10-25 23:33:27 +0300

    Initial commit

---
+++ src/main/kotlin/com/mak7chek/carexpenses/api/CarExpensesApiApplication.kt
@@ -0,0 +1,11 @@
+package com.mak7chek.carexpenses.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CarExpensesApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<CarExpensesApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/mak7chek/carexpenses/api/CarExpensesApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mak7chek.carexpenses.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CarExpensesApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
